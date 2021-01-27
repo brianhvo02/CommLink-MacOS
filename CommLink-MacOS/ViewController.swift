d4d92b776a92cf9d56a5764a5f490dd71f9cabbf
Brian Vo <brian.vo@sjsu.edu>
2021-01-27 12:33:54 -0800

    Initial Commit

---
+++ CommLink-MacOS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CommLink-MacOS
+//
+//  Created by Brian Huy Vo on 1/27/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
